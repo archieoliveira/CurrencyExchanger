ce1610508ed2fa40dc5b6e8053674e6270033aeb
Artur Oliveira <arturolivv@gmail.com>
2024-09-07 17:13:12 -0300

    third commit

---
+++ Controller/ConverterController.cs
@@ -0,0 +1,24 @@
+﻿namespace ConverterAPI.Controller
+{
+    public class ConverterController 
+    {
+        public async Task<IResult> Get(string from, string to, decimal amount, IHttpClientFactory httpClientFactory)
+        {
+            var client = httpClientFactory.CreateClient("CurrencyApi");
+            var response = await client.GetAsync($"{from}");
+
+            if (!response.IsSuccessStatusCode)
+                return Results.Problem("Erro ao obter taxa de câmbio...");
+
+            var rates = await response.Content.ReadFromJsonAsync<ExchangeRates>();
+
+            if (rates is null || !rates.ConversionRate.ContainsKey(to))
+                return Results.Problem("Taxa de cambio não encontrada...");
+
+            var rate = rates.ConversionRate[to];
+            var convertedAmount = amount * rate;
+
+            return Results.Ok(new { from, to, amount, convertedAmount });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using ConverterAPI.Controller;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,25 +19,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapGet("/converter", async (string from, string to, decimal amount, IHttpClientFactory httpClientFactory) =>
-{
-    var client = httpClientFactory.CreateClient("CurrencyApi");
-    var response = await client.GetAsync($"{from}");
-
-    if (!response.IsSuccessStatusCode)
-        return Results.Problem("Erro ao obter taxa de câmbio...");
-
-    var rates = await response.Content.ReadFromJsonAsync<ExchangeRates>();
-        
-    if (rates is null || !rates.ConversionRate.ContainsKey(to))
-        return Results.Problem("Taxa de cambio não encontrada...");
-
-    var rate = rates.ConversionRate[to];
-    var convertedAmount = amount * rate;
-
-    return Results.Ok(new {from, to, amount, convertedAmount});
-    
-});
+app.MapGet("/converter", async (string from, string to, decimal amount, IHttpClientFactory httpClientFactory) => await new ConverterController().Get(from, to, amount, httpClientFactory));
 
 app.Run();
 
