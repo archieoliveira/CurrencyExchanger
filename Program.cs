57bdecc84350c071d147c4f21b64c28aa8335cd4
Artur Oliveira <arturolivv@gmail.com>
2025-06-13 00:48:45 -0300

    alterando url do git no yml

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient("CurrencyApi", client =>
 {
     client.BaseAddress = new Uri($"https://v6.exchangerate-api.com/v6/{builder.Configuration["ApiKey"]}/latest/");
-}); // testando alteraando path csproj tirando a /solution corrigindo o pat
+}); // corrigindo url do git no yml
 
 var app = builder.Build();
 
