56f0a16b11f60bd81096ff4998b43c4f13641f83
Artur Oliveira <arturolivv@gmail.com>
2025-06-13 00:11:09 -0300

    testando alteração no yml

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient("CurrencyApi", client =>
 {
     client.BaseAddress = new Uri($"https://v6.exchangerate-api.com/v6/{builder.Configuration["ApiKey"]}/latest/");
-}); // testando commit na pipeline
+}); // testando commit na pipeline 2
 
 var app = builder.Build();
 
