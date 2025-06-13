9f6b6ec060c053581f87255d96197da1137db970
Artur Oliveira <arturolivv@gmail.com>
2025-06-12 23:39:12 -0300

    alterando a porta da api

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient("CurrencyApi", client =>
 {
-    client.BaseAddress = new Uri($"https://v6.exchangerate-api.com/v6/{builder.Configuration["ApiKey"]}/latest/"); // url api teste commit
+    client.BaseAddress = new Uri($"https://v6.exchangerate-api.com/v6/{builder.Configuration["ApiKey"]}/latest/"); // url api teste commit alterando porta
 });
 
 var app = builder.Build();
