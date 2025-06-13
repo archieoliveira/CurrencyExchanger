2c5b2094f3c87a0e50c1e9c2883460461209c16a
Artur Oliveira <arturolivv@gmail.com>
2025-06-13 00:25:49 -0300

    corrigindo o path /solution

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient("CurrencyApi", client =>
 {
     client.BaseAddress = new Uri($"https://v6.exchangerate-api.com/v6/{builder.Configuration["ApiKey"]}/latest/");
-}); // testando alteraando path csproj
+}); // testando alteraando path csproj colocando solution
 
 var app = builder.Build();
 
